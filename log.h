03999735e7995672d3c77a52a1670b80126d60cb
mattison chao <mattisonchao@gmail.com>
2022-08-03 23:18:49 +0800

    step commit

---
+++ log.h
@@ -0,0 +1,10 @@
+//
+// Created by mattison chao on 2022/8/3.
+//
+
+#ifndef CPLUSPLUS_LOG_H
+#define CPLUSPLUS_LOG_H
+
+void log(const char *messages);
+
+#endif //CPLUSPLUS_LOG_H
---
+++ simple.cpp
@@ -0,0 +1,10 @@
+//
+// Created by mattison chao on 2022/8/3.
+//
+#include <iostream>
+#include "log.h"
+
+int main() {
+    log("Hello world");
+    std::cin.get();
+}
